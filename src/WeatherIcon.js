41208cace666b8b48f9b763725363398e9aeed3f
Phoebe W <phoebe.wilckens@gmail.com>
2020-12-30 12:12:55 -0300

    change of color

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -25,7 +25,7 @@ export default function WeatherIcon(props){
 
 return (  <ReactAnimatedWeather
     icon={codeMapping[props.code]}
-    color="#a5ecd7"
+    color="#333333"
     size={64}
     animate={true}
   />);
